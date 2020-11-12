743be9d9cdc0bf7a5f11f4a4113322bc6bd3b9c5
Rittik Kumar <rkrittik17@gmail.com>
2020-11-12 08:50:14 +0530

    Starting the project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    // BEM class naming convention
+    <div className="app">
+      <h1>Lets build MERN Tinder Clone !</h1>
+      
+      <Header />
+      {/*tinderCards*/}
+      {/*swiperButton*/}
+
+
+    </div>
+  );
+}
+
+export default App;
