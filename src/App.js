cded265990b0fc8531d5e032364d911ecb9f0025
Rittik Kumar <rkrittik17@gmail.com>
2020-11-18 21:16:35 +0530

    Making swipe buttons

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from "react";
 import './App.css';
 import Header from "./Header";
 import TinderCards from "./TinderCards";
+import SwipeButtons from "./SwipeButtons";
 function App() {
   return (
     // BEM class naming convention
     <div className="app">
       <Header />
       <TinderCards />
-      {/*swiperButton*/}
-
-
+      <SwipeButtons />
     </div>
   );
 }
