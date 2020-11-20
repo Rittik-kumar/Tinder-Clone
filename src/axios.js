82b6a30b3d8995c3fe16f316fc8019a67e847667
Rittik Kumar <rkrittik17@gmail.com>
2020-11-20 18:05:11 +0530

    .

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://tnder-clone.herokuapp.com'
+  baseURL: "https://tnder-clone.herokuapp.com",
 })
 
 export default instance;
